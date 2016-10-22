103a95eae2a20888d8a2340be05311f5aa2ba0a7
Mark Abidargham <markabid@users.noreply.github.com>
2016-10-22 00:00:38 -0400

    added values from spinner selections

--- PerfectToiletTime/app/src/main/java/com/perfecttoilettime/perfecttoilettime/MailSenderActivity.java
+++ PerfectToiletTime/app/src/main/java/com/perfecttoilettime/perfecttoilettime/MailSenderActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Spinner;
 
 public class MailSenderActivity extends AppCompatActivity {
 
@@ -13,13 +14,22 @@ public class MailSenderActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mail_sender);
 
+        Spinner buildingSpinner = (Spinner) findViewById(R.id.buildingSpinner);
+        String building = buildingSpinner.getSelectedItem().toString();
+
+        Spinner floorSpinner = (Spinner) findViewById(R.id.floorSpinner);
+        String floor = floorSpinner.getSelectedItem().toString();
+
+        Spinner problemSpinner = (Spinner) findViewById(R.id.problemSpinner);
+        String problem = problemSpinner.getSelectedItem().toString();
+
         Button sendMailBtn = (Button) findViewById(R.id.sendButton);
         sendMailBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 try{
                     GMailSender sender = new GMailSender("perfecttoilettimeapp@gmail.com","Toiletries*");
-                    sender.sendMail("This is subject", "This is body", "perfecttoilettime@gmail.com", "mark.yankees@gmail.com");
+                    sender.sendMail("Bathroom Service Request", "Bathroom needs service", "perfecttoilettime@gmail.com", "mark.yankees@gmail.com");
                 } catch (Exception e){
                     Log.e("SendMail", e.getMessage(), e);
                 }
