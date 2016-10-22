be4fb39c0a01c594eb93ad28240ffc09177ea2cc
Mark Abidargham <markabid@users.noreply.github.com>
2016-10-21 23:44:01 -0400

    added jar files and code to send email, not yet functional

--- PerfectToiletTime/app/src/main/java/com/perfecttoilettime/perfecttoilettime/MailSenderActivity.java
+++ PerfectToiletTime/app/src/main/java/com/perfecttoilettime/perfecttoilettime/MailSenderActivity.java
@@ -2,6 +2,9 @@ package com.perfecttoilettime.perfecttoilettime;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MailSenderActivity extends AppCompatActivity {
 
@@ -9,5 +12,19 @@ public class MailSenderActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mail_sender);
+
+        Button sendMailBtn = (Button) findViewById(R.id.sendButton);
+        sendMailBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try{
+                    GMailSender sender = new GMailSender("username@gmail.com","password");
+                    sender.sendMail("This is subject", "This is body", "user@gmail.com", "user@gmail.com");
+                } catch (Exception e){
+                    Log.e("SendMail", e.getMessage(), e);
+                }
+
+            }
+        });
     }
 }
