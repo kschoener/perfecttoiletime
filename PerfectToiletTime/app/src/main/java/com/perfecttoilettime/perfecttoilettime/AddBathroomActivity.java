7204befea10d8d8ce6ca5fc083c2e80bc824f433
Mark Abidargham <markabid@users.noreply.github.com>
2016-10-25 22:18:29 -0400

    adds new bathroom to database

--- PerfectToiletTime/app/src/main/java/com/perfecttoilettime/perfecttoilettime/AddBathroomActivity.java
+++ PerfectToiletTime/app/src/main/java/com/perfecttoilettime/perfecttoilettime/AddBathroomActivity.java
@@ -7,6 +7,15 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
 
+import java.io.BufferedInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+
 
 public class AddBathroomActivity extends AppCompatActivity {
 
@@ -30,7 +39,36 @@ public class AddBathroomActivity extends AppCompatActivity {
                 String description = bathroomDescription.getText().toString(); //description of bathroom from text field
                 String longitude = ""; //longitude of bathroom location(always empty for now until figure out how to implement this)
                 String latitude = ""; //latitude of bathroom location(always empty for now until figure out how to implement this)
+                String urlString ="http://socialgainz.com/Bumpr/PerfectToiletTime/insertBathroom.php?longitude=" +longitude+ "&latitude=" +latitude+ "&name=" +name+ "&description=" +description+ "";
+                final String urlString1 = urlString.replaceAll(" ", "%20");
+                System.out.println(urlString1);
+                try
+                {
+                    URL url = new URL(urlString1);
+                    HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+                    InputStream in = new BufferedInputStream(urlConnection.getInputStream());
+                    String re = readStream(in);
+                }
+                catch(Exception e)
+                {
+                    e.printStackTrace();
+                }
             }
         });
+    }
+    private String readStream(InputStream is){
+        try {
+            ByteArrayOutputStream bo = new ByteArrayOutputStream();
+            int i = is.read();
+            while (i != -1) {
+                bo.write(i);
+                i = is.read();
+            }
+            return bo.toString();
+        }
+        catch (IOException e){
+            return "";
+
+        }
     }
 }
