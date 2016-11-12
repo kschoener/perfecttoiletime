f63137119d0ca0fd0e73c59110d92e2d2fa71956
juno <junocongying@gmail.com>
2016-11-12 15:08:10 -0500

    rating bar basic function, need tobe fixed(rating bar lisener) change the female background color

--- PerfectToiletTime/app/src/main/java/com/perfecttoilettime/perfecttoilettime/frontEnd/RatingActivity.java
+++ PerfectToiletTime/app/src/main/java/com/perfecttoilettime/perfecttoilettime/frontEnd/RatingActivity.java
@@ -1,8 +1,11 @@
 package com.perfecttoilettime.perfecttoilettime.frontEnd;
 
 import android.content.Intent;
+import android.support.v4.content.res.ResourcesCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.RatingBar;
 import android.widget.TextView;
 
@@ -21,11 +24,11 @@ public class RatingActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        int[] prevPref = null;
+        int[] prevRate = null;
         Intent startIntent = getIntent();
 
         if(startIntent.getExtras().containsKey(ratingExtraKey)){
-            prevPref = startIntent.getExtras().getIntArray(ratingExtraKey);
+            prevRate = startIntent.getExtras().getIntArray(ratingExtraKey);
         }
         ratingBar = new ArrayList<RatingBar>();
         rateText = new ArrayList<TextView>();
@@ -33,6 +36,61 @@ public class RatingActivity extends AppCompatActivity {
         for(int i =1; i <= 4; i++){
             String barID = "ratingBar";
             String textID = "text";
+            barID += i;
+            textID += i;
+            int barResID = getResources().getIdentifier(barID, "id", getPackageName());
+            int textResID = getResources().getIdentifier(textID, "id", getPackageName());
+
+            RatingBar tempBar = (RatingBar) findViewById(barResID);
+            if(prevRate != null){
+                tempBar.setProgress(prevRate[i-1]);
+                
+            }
+            TextView tempText = (TextView) findViewById(textResID);
+            tempText.setText(tempBar.getProgress() + "/" + tempBar.getMax());
+            tempBar.setOnRatingBarChangeListener(MyRateListener);
+
+            ratingBar.add(tempBar);
+            rateText.add(tempText);
+
+        }
+        Button saveButton = (Button) findViewById(R.id.rateSave);
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent startMap = new Intent(v.getContext(), MapsActivity.class);
+//                int[] extras = new int[RatingBar.resolveSize()];
+//                for(int i = 0; i < RatingBar.size(); i += 1){
+                    //todo add save functionality for Rating
+//                    extras[i] = RatingBar.get(i).getProgress();
+//                    startMap.putExtra((String) prefNames.get(i).getText(), bars.get(i).getProgress());
+//               }
+                startMap.putExtra(ratingExtraKey, extras);
+                startMap.putExtra(genderActivity.genderExtraKey, gender);
+                startActivity(startMap);
+            }
+        });
+        if(startIntent.getExtras().containsKey(genderActivity.genderExtraKey)){
+            gender = startIntent.getExtras().getInt(genderActivity.genderExtraKey);
+            switch (gender){
+                case genderActivity.maleValue:
+                    (findViewById(R.id.activity_preferences)).setBackgroundColor(
+                            ResourcesCompat.getColor(getResources(), R.color.maleBackgroundColor, null));
+                    break;
+                case genderActivity.femaleValue:
+                    (findViewById(R.id.activity_preferences)).setBackgroundColor(
+                            ResourcesCompat.getColor(getResources(), R.color.femaleBackgroundColor, null));
+                    break;
+            }
         }
     }
+
+    private RatingBar.OnRatingBarChangeListener MyRateListener = new RatingBar.OnRatingBarChangeListener(){
+        public void onRatingChanged(RatingBar ratingBar, float rating,
+                                    boolean fromUser) {
+
+            // TODO: 11/12/16 to finish up the rating bar listener
+        }
+
+    };
 }
